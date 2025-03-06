4d8eef40c3f636177a5142fa9b0a82a9d4371fac
kaushik132 <kaushik.dey.ahit@gmail.com>
2025-03-06 13:14:32 +0530

    new

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('dashboard.layout.main')
+@section('main-containers')
+
+
+<div class="flex flex-col items-center justify-center h-screen">
+   {{-- <img src="{{url('assets/images/404.jpg')}}" alt="404" class="w-1/2 md:w-1/3 lg:w-1/4"> --}}
+   <h1 class="text-4xl font-bold mt-10">Page Not Found</h1>
+</div>
+
+@endsection
